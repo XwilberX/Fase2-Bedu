10f4f4744e5ee9cc1bb2e3b9c40d88a4b45ed4f0
WilberA <wilber.alegria99@gmail.com>
2021-01-16 15:48:46 -0600

    Commit1
    Primer commit

---
+++ Sesion1/PostworkS1.r
@@ -0,0 +1,16 @@
+Datos<-read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv", header=T)
+FTHG<-Datos$FTHG
+FTAG<-Datos$FTAG
+
+##Inciso 1
+Prob_FTHG<-table(FTHG)/length(FTHG)
+
+##Inciso 2
+Prob_FTAG<-table(FTAG)/length(FTAG)
+
+##Inciso 3
+#Columnas con los goles del equipo visitante y las filas los goles del equipo local
+Prob_Conjunta<-table(as.data.frame(cbind(FTHG,FTAG)))/length(FTAG)
+Contraer
+
+
